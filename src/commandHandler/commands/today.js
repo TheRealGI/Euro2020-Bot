1755bce39f2c81fad047b14620b0dc44c29adde0
Dominic Gilomen <dominic.gilomen@gmail.com>
2021-06-04 20:43:32 +0200

    rm sqlite file, updated today command,

--- src/commandHandler/commands/today.js
+++ src/commandHandler/commands/today.js
@@ -1,24 +1,48 @@
 const moment = require('../../util/date');
 const matchService = require('../../service/match-service');
+const StateDto = require('../../db/dto/StateDto');
+const Discord = require("discord.js");
 module.exports = {
     name: 'today',
     needsArgs: false,
-     async execute(message, args) {
-        var msg = ""; 
+    async execute(message, args) {
         let dateToday = new Date();
-        moment.getUtcDate(dateToday.setDate(dateToday.getDate()))
+        moment.getUtcDate(dateToday.setDate(dateToday.getDate() + 7))
 
-        var matchesToday =  await matchService.getMatchesByDate(dateToday);
+        var matchesToday = await matchService.getMatchesByDate(dateToday);
 
-        if(!matchesToday || matchesToday.length < 1) {
+        if (!matchesToday || matchesToday.length < 1) {
             return message.channel.send(`No match found for today`)
         }
-         msg = buildMessage(msg, matchesToday);
-         return message.channel.send(msg);
+        let embed = buildMessage(matchesToday);
+
+        return message.channel.send(embed);
     }
 }
 
-function buildMessage(msg, matches) {
-      matches.map(x => msg+= "\uFFED " + "\xa0\xa0\xa0\xa0" + x.HomeName + ' vs. ' + x.AwayName+ '   ' + moment.getUTCTimestamp(x.Timestamp) + "\n" )
-      return msg;
+function buildMessage( matches) {
+    let msg = "";
+    let state = new StateDto();
+    matches.map(x => {
+        switch (x.Status) {
+            case state.SCHEDULED:
+                msg += x.HomeName + ' vs. ' + x.AwayName + '   ' + moment.getUTCTimestamp(x.Timestamp) + "\n";
+                break;
+
+            case state.INPLAY:
+                msg += "\uFFED " + x.HomeName + ' vs. ' + x.AwayName + '   ' + x.HomeScore + ' : ' + x.AwayScore + "\n";
+                break;
+
+            case state.FINISHED:
+                msg += "Finished\t" + x.HomeName + ' vs. ' + x.AwayName + ' -->  ' + x.HomeScore + ' : ' + x.AwayScore + "\n";
+                break;
+
+            default:
+                break;
+        }
+    });
+    let embed = new Discord.MessageEmbed()
+        .setColor(0x5ee59d)
+        .addField('Todays schedule:', msg);
+    return embed;
 }
---
+++ src/db/dto/StateDto.js
@@ -0,0 +1,7 @@
+class StateDto {
+     SCHEDULED = "SCHEDULED";
+     INPLAY = "IN_PLAY";
+     FINISHED = "FINISHED";
+}
+
+module.exports = StateDto;
