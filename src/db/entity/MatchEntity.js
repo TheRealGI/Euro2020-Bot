446bf69ccb7fa737e0b19aca4a60a00e9d412f9e
Dominic Gilomen <dominic.gilomen@gmail.com>
2021-06-11 22:09:12 +0200

    replace text with emoji

--- src/db/entity/MatchEntity.js
+++ src/db/entity/MatchEntity.js
@@ -15,8 +15,63 @@ class MatchEntity {
         this.Duration = match.duration;
         this.HomeScore = match.score.fullTime.homeTeam;
         this.AwayScore = match.score.fullTime.awayTeam;
-        this.HomeName = match.homeTeam.name;
-        this.AwayName = match.awayTeam.name;
+        this.HomeName = replaceNameWithCode(match.homeTeam.name);
+        this.AwayName = replaceNameWithCode(match.awayTeam.name);
+    }
+}
+
+function replaceNameWithCode(country) {
+    switch(country){
+        case "Turkey":
+            return ":flag_tr:";
+        case "Italy":
+            return ":flag_it:";
+        case "Wales":
+            return ":wales:";
+        case "Switzerland":
+            return ":flag_ch:";
+        case "Denmark": 
+            return ":flag_dk:";
+        case "Finland":
+            return ":flag_fi:";
+        case "Belgium":
+            return ":flag_be:";
+        case "Russia":
+            return ":flag_ru:";
+        case "England":
+            return ":england:";
+        case "Croatia":
+            return ":flag_hr:";
+        case "Austria":
+            return  ":flag_at:";
+        case "North Macedonia":
+            return  ":flag_mk:";
+        case "Netherlands":
+            return  ":flag_nl:";
+        case "Ukraine":
+            return  ":flag_ua:";
+        case "Scotland":
+            return ":scotland:";
+        case "Czech Republic":
+            return  ":flag_cz:";
+        case "Poland":
+            return  ":flag_pl:";
+        case "Slovakia":
+            return ":flag_sk:";
+        case "Spain":
+            return  ":flag_es:";
+        case "Sweden":
+            return ":flag_se:";
+        case "Hungary":
+            return ":flag_hu:";
+        case "Portugal":
+            return ":flag_pt:";
+        case "France":
+            return ":flag_fr:";
+        case "Germany":
+            return  ":flag_de:";
+        default:
+            return country;
     }
 }
 
